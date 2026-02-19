aaccc2b2bff931d28dc8f57bf9cbd9d7b4fa6e0a
kowsikanA <kowsikan.a@gmail.com>
2026-02-18 19:10:19 -0500

    did sqflite localdb of expenses

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,29 +1,37 @@
 class Expense{
     int? id;
-    String? title;
-    double? amount;
+    String title;
+    double amount;
     DateTime? date;
     String? description;
-    String? paymentMethod;
+    String paymentMethod;
 
-    Expense({this.id, required this.title, required this.amount, this.date, this.description, required this.paymentMethod});
+    Expense({
+            this.id, 
+            required this.title, 
+            required this.amount, 
+            this.date, 
+            this.description, 
+            required this.paymentMethod
+          });
 
-    Expense.fromMap(Map<String, dynamic> map){
-        this.id = map["id"];
-        this.title = map["title"];
-        this.amount = map["amount"];
-        this.date = map["date"];
-        this.description = map['description'];
-        this.paymentMethod = map['paymentMethod'];
+    Expense.fromMap(Map<String, dynamic> map)
+    : id = map["id"] as int?,
+      title = map["title"] as String,
+      amount = (map["amount"] as num).toDouble(),
+      date = map["date"] != null
+          ? DateTime.parse(map["date"] as String)
+          : null,
+      description = map["description"] as String?,
+      paymentMethod = map["paymentMethod"] as String;
 
-    }
 
     Map<String, dynamic> toMap() {
         return{
             'id' :  this.id,
             'title' : this.title,
             'amount' : this.amount,
-            'date' : DateTime.now(),
+            'date' : date?.toIso8601String(),
             'description':  this.description,
             'paymentMethod' : this.paymentMethod
         };
